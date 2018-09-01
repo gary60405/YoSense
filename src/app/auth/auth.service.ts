d9c2d72baffa0cc79d33aefacbaabc05d7dc30ab
Gary <gary60405@gmail.com>
2018-09-01 15:24:16 +0800

    導入ngrx

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFirestore } from 'angularfire2/firestore';
-import * as firebase from 'firebase/app';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
